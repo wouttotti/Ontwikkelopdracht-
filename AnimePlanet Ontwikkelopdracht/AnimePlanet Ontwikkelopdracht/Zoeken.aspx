05d0c036b0a0021ab14ec3e5c9f25045b3e96556
wouttotti <wout_kamp@hotmail.com>
2015-06-04 19:09:09 +0200

    Login functie werkend
    De login in fuctie is compleet en werkt zoals gewenst.

--- AnimePlanet Ontwikkelopdracht/AnimePlanet Ontwikkelopdracht/Zoeken.aspx
+++ AnimePlanet Ontwikkelopdracht/AnimePlanet Ontwikkelopdracht/Zoeken.aspx
@@ -2,4 +2,14 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <link href="Stylesheet.css" rel="stylesheet" type="text/css" />
+    <div id="Zoeken" style="margin-left: 100px">
+    <asp:TextBox ID="TbZoeken" runat="server" style="width: 300px"></asp:TextBox>
+    <asp:DropDownList ID="DdlSoort"  runat="server">
+        <asp:ListItem>Anime</asp:ListItem>
+        <asp:ListItem>Manga</asp:ListItem>
+        <asp:ListItem>Personage</asp:ListItem>
+    </asp:DropDownList>
+    </div>
 </asp:Content>
+
--- AnimePlanet Ontwikkelopdracht/AnimePlanet Ontwikkelopdracht/Zoeken.aspx.cs
+++ AnimePlanet Ontwikkelopdracht/AnimePlanet Ontwikkelopdracht/Zoeken.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace AnimePlanet_Ontwikkelopdracht
 {
@@ -11,7 +12,26 @@ namespace AnimePlanet_Ontwikkelopdracht
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                bindGridview_mouse_over_detailedData();
+            }
+        }
+        public void bindGridview_mouse_over_detailedData()
+        {
+            // Created a new datatable
+            DataTable dt = new DataTable();
 
+            // Added columns to thedatatable
+            dt.Columns.Add("item_id", typeof(string));
+            dt.Columns.Add("titel", typeof(string));
+            dt.Columns.Add("score", typeof(string));
+            dt.Columns.Add("PictureUrl", typeof(string));
+            dt.Columns.Add("Jaar", typeof(string));
+
+            dt.Rows.Add("4", "Pandora Hearts", "4.1", @"C:\Users\Wout\Documents\GitHub\Ontwikkelopdracht-\AnimePlanet Ontwikkelopdracht\AnimePlanet Ontwikkelopdracht\Images\pandora_hearts.jpg", "2009");
+            gv_MouseOverData.DataSource = dt;
+            gv_MouseOverData.DataBind();
         }
     }
 }
